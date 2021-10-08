136db4a94ca70f3af2612bc71f08a5e93a1cb938
sleepingsaint <suryasantosh14523@gmail.com>
2021-10-08 22:27:05 +0530

    changed file paths in download script

--- download.py
+++ download.py
@@ -7,10 +7,10 @@ import shutil
 
 # file ids
 links = {
-    "1C3Kqqu9gDXNNXr5WDpmhGTr-UaQ2ckqQ": ["oldobj_detection.weights", "detector/YOLOv3/weight/"],
+    "1C3Kqqu9gDXNNXr5WDpmhGTr-UaQ2ckqQ": ["obj_detection.weights", "detector/YOLOv3/weight/"],
     "1VfRVvc-7EowI540S0_6FxJOMVg9XyXef": ["traj_arm.pth", "Trajectory/models/Individual/"],
     "1t_qok3BNNHN6EK_Uw3WiXrfGtLfQJpCs": ["traj_endeffector.pth", "Trajectory/models/Individual/"],
-    "1SEZGUvLB2gfVwA-yGTBlF5k3YDiOzYWj": ["traj_probe_holder.pth", "Trajectory/models/Individual/"],
+    "1SEZGUvLB2gfVwA-yGTBlF5k3YDiOzYWj": ["traj_probe.pth", "Trajectory/models/Individual/"],
     "1p8vo9rig9Q0i0WLVudQBgzkdjtJXcDiS": ["00013.pth", "Trajectory/models/Individual/eth_train/"],
     "1MlXnCSjD5yOfxnnJMkruE0rCgLCMZJlB": ["ckpt.t7", "deep_sort/deep/checkpoint/"],
     "13mVaJTsJ7rN-Bz5KtsS20dX1TZFAHLkU": ["test_video.mp4", "data/"]
